64600ffadb37577803d51c07bb3d1a55af177f59
subodh765444 <221b396@juetguna.in>
2025-05-08 11:08:05 +0530

    Add files via upload

---
+++ LAB 8 (ADAPTER)/IndianCrow.java
@@ -0,0 +1,11 @@
+public class IndianCrow implements Crow {
+    public void cry(){
+        System.out.println("cawing");
+    }
+    public void fly(){
+        System.out.println("flying high over the mountains");
+    }
+    public void eat(){
+        System.out.println("eating Pearls");
+    }
+}
